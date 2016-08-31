d0de301f73f0f30adcdd86182d3525eab75aeb52
Dillon Lundell <dillonjaylundell@gmail.com>
2016-08-31 15:36:58 -0500

    set.contains complete

--- Data_Structures/set.js
+++ Data_Structures/set.js
@@ -11,3 +11,9 @@ setPrototype.insert = function(value) {
 	// add the the value as a property on the storage object.
 	this.storage[value] = true;
 }
+
+setPrototype.contains = function(value){
+	// Check for the search value in the storage object. 
+	// Will return true or false as when we add values we add them as properties. 
+	return this.storage.hasOwnProperty(value);
+}
