33041e50eab4c8470738087fe9fd25c6656f2d9b
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-01 08:23:32 -0500

    Stack with array storage push function complete

---
+++ Data_Structures/stack.js
@@ -0,0 +1,17 @@
+// Using an array as storage.
+function Stack(){
+	this.storage = [];
+}
+
+Stack.prototype.push = function(value){
+	// push the value into the storage array. 
+	this.storage.push(value);
+}
+// Using an object as storage.
+
+
+
+
+
+
+
