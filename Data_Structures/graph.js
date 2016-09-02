6b223ec22415c619ae32fdd4626c5079a1bf7c41
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-02 08:57:11 -0500

    contains function complete

--- Data_Structures/graph.js
+++ Data_Structures/graph.js
@@ -8,9 +8,13 @@ function Graph() {
 };
 
 var graphMethods = {};
-
-graphMethods.addNode = function() {
-	// Add the node as a property on the graph.
+// Add a node to the graph.
+graphMethods.addNode = function(node) {
 	// Each node in the graph will have an object to store edges. 
 	this.nodes[node] = { edges : {} }
+};
+// Check to see if the graph contains a specific node.
+graphMethods.contains = function(node) {
+	// Return the boolean value for weather or not the node we are searching for exists.
+	return Boolean(this.nodes[node]);
 }
