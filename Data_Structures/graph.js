f1f8e032ee9e028aa54e34d10dcf0a4951ff22c4
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-02 09:16:28 -0500

    hasEdge function complete

--- Data_Structures/graph.js
+++ Data_Structures/graph.js
@@ -11,7 +11,7 @@ var graphMethods = {};
 // Add a node to the graph.
 graphMethods.addNode = function(node) {
 	// Each node in the graph will have an object to store edges. 
-	this.nodes[node] = { edges : {} }
+	this.nodes[node] = { edges : {} };
 };
 // Check to see if the graph contains a specific node.
 graphMethods.contains = function(node) {
@@ -22,12 +22,17 @@ graphMethods.contains = function(node) {
 graphMethod.removeNode = function(node) {
 	//first we check to see if the node exists.
 	if(this.nodes[node]){
-		//If it does then we wil remove all connection to any other nodes before deleting. 
-		for(var otherNode in this.nodes[node].edges){
+		//If it does then we wil remove all connections to any other nodes before deleting. 
+		for(var otherNode in this.nodes[node].edges) {
 			this.removeEdge(node, otherNode);
 		};
 		// Then delete.
 		delete this.nodes[node];
 	};
 };
-
+// Check to see if two nodes are connected.
+graphMethods.hasEdge = function(fromNode, toNode) {
+	// Does fromNode exist ? If so, see if toNode is one of its edges.
+	if(this.nodes[fromNode]) return Boolean(this.nodes[fromNode].edges[toNode])
+};
+// Adds a connection between two nodes.
