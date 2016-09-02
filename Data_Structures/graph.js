e0f443f8bdd94fd3755e4e2a5143ed3d49622ca0
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-02 08:49:47 -0500

    addNode function complete

--- Data_Structures/graph.js
+++ Data_Structures/graph.js
@@ -2,8 +2,15 @@
 
 // Prototypal
 function Graph() {
-	var node = Object.create(graphMethods)
-	return node
+	var graph = Object.create(graphMethods);
+	graph.nodes = {};
+	return graph;
 };
 
 var graphMethods = {};
+
+graphMethods.addNode = function() {
+	// Add the node as a property on the graph.
+	// Each node in the graph will have an object to store edges. 
+	this.nodes[node] = { edges : {} }
+}
