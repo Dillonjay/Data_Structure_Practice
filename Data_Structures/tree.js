e5dabf6dee27430c1917ec6e606145a2b4fda39b
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-03 19:36:25 -0500

    contains function complete

--- Data_Structures/tree.js
+++ Data_Structures/tree.js
@@ -1,5 +1,5 @@
 // Prototypal instantiation.
-var Tree = function(value){
+var Tree = function(value) {
 	var newTree = Object.create(treeMethods);
 	newTree.children = [];
 	newTree.value = value;
@@ -7,8 +7,21 @@ var Tree = function(value){
 };
 
 var treeMethods = {};
-
-treeMethods.addChild = function(value){
+// Adds a child to a tree.
+treeMethods.addChild = function(value) {
+	// Push a brand new tree into the children array. 
 	this.children.push(Tree(value));
 };
-
+// Checks if the tree contains a value you are looking for. 
+treeMethods.contains = function(target) {
+	// If the current trees value === target return true.
+	if (this.value === target) return true;
+	// Set an answer variable to false and return it at the end.
+	var answer = false;
+	// Loop through the current trees children.
+	this.children.forEach(tree => {
+		// Recurse with the contains method. If the target is found, set the answer to true.
+		tree.contains(target) ? answer = true : null;
+	})
+	return answer;
+};
