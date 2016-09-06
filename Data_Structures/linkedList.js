02ad6a1cebc4a07712eedf620abb0bde89a96fbf
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-05 20:06:12 -0500

    Linked List set up

--- Data_Structures/linkedList.js
+++ Data_Structures/linkedList.js
@@ -7,3 +7,32 @@ function link(value) {
 	link.next = null;
 	return link;
 };
+
+// Prototypal Instantiation.
+function LinkedList() {
+	var list = Object.create(linkedListMethods);
+	list.head = null;
+	list.tail = null;
+	return list;
+};
+
+linkedListMethods = {};
+// Add a new link to tail. 
+linkedListMethods.addToTail = function(value) {
+
+};
+// Remove the first link in the list.
+linkedListMethods.removeHead = function() {
+
+};
+// Check to see if the list contains a value.
+linkedListMethods.contains = function(target) {
+
+};
+// Display every value in the list.
+linkedListMethods.display = function() {
+
+};
+
+
+
