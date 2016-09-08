b3995875f75462173c42877abdae71fc61871e7a
Dillon Lundell <dillonjaylundell@gmail.com>
2016-09-08 12:11:40 -0500

    get function for storage methods complete

--- Data_Structures/hashTable.js
+++ Data_Structures/hashTable.js
@@ -2,40 +2,45 @@
 // Hashing function for creating a hash.
 // Took Hash function from the internet.  
 var hashFun = function(str, max) {
-  var hash = 0;
-  for (var i = 0; i < str.length; i++) {
-    hash = (hash << 5) + hash + str.charCodeAt(i);
-    hash = hash & hash; // Convert to 32bit integer
-    hash = Math.abs(hash);
-  }
-  return hash % max;
+	var hash = 0;
+	for (var i = 0; i < str.length; i++) {
+    	hash = (hash << 5) + hash + str.charCodeAt(i);
+    	hash = hash & hash; // Convert to 32bit integer
+    	hash = Math.abs(hash);
+  	};
+	return hash % max;
 };
 
 //The special storage array for buckets.
 var storageArray = function(limit) {
-  var storage = [];
+	// Storage for storing buckets.
+	var storage = [];
 
-  var storageArray = {};
+	// Methods for interacting with the sorage array.
+	var storageArray = {};
 
-  storageArray.get = function(index) {
-  	
-  };
-  storageArray.set = function(index, value) {
+	// Return the bucket at index.
+	storageArray.get = function(index) {
+  		checkLimit(index);
+  		//return the bucket.
+  		return storage[index];	
+    };
+	storageArray.set = function(index, value) {
    
-  };
-  storageArray.each = function(callback) {
+    };
+	storageArray.each = function(callback) {
   
    
-  };
+    };
 
-  var checkLimit = function(index) {
-  	// Make sure that the index is actually a number.
-  	if(index.constructor !== Number) throw new Error('Argument must be a number');
-  	// Make sure the index is below the storage limit. 
-  	if(index >= limit) throw new Error('Error, trying to access an index that is over the limit');
-  };
+	var checkLimit = function(index) {
+  		// Make sure that the index is actually a number.
+  		if(index.constructor !== Number) throw new Error('Argument must be a number');
+  		// Make sure the index is below the storage limit. 
+  		if(index >= limit) throw new Error('Error, trying to access an index that is over the limit');
+	};
 
-  return storageArray;
+	return storageArray;
 };
 // Pseudoclassical instantiation
 var HashTable = function() {
